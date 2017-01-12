256797df218c0d96a3ee2b7f56687f9f07dd89a0
Aaron Marburg <amarburg@apl.washington.edu>
2017-01-11 16:44:14 -0800

    Added brief benchmarking code.

--- lazyquicktime_test.go
+++ lazyquicktime_test.go
@@ -9,6 +9,7 @@ import "os"
 import "github.com/amarburg/go-lazyfs"
 
 import "image/png"
+import "github.com/amarburg/go-lazyfs-testfiles"
 import "github.com/amarburg/go-lazyfs-testfiles/http_server"
 
 
@@ -16,20 +17,21 @@ import "github.com/amarburg/go-lazyfs-testfiles/http_server"
 //var TestUrlRoot = "https://amarburg.github.io/go-lazyfs-testfiles/"
 //var TestUrlRoot = "http://localhost:8080/files/"
 //var TestUrl,_ = url.Parse( TestUrlRoot + TestMovPath )
-var TestMovPath = "CamHD_Vent_Short.mov"
+//var TestMovPath = "CamHD_Vent_Short.mov"
 
 
 // For local testing
 //var TestMovPath = lazyfs_testfiles.TestMovPath
 
 
-var SparseHttpStoreRoot = "cache/httpsparse/"
+var SparseHttpStoreRoot = "test_cache/httpsparse/"
 
 func TestConvert( t *testing.T ) {
 
-  srv := lazyfs_testfiles.HttpServer( 4567 )
+  srv := lazyfs_testfiles_http_server.HttpServer( 4567 )
+  defer srv.Stop()
 
-  testUrl,err := url.Parse( "http://localhost:4567/" + TestMovPath )
+  testUrl,err := url.Parse( "http://localhost:4567/" + lazyfs_testfiles.TestMovFile )
 
   source,err := lazyfs.OpenHttpSource( *testUrl )
   //fmt.Println(source)
@@ -59,5 +61,45 @@ func TestConvert( t *testing.T ) {
   if err != nil { panic(fmt.Sprintf("Error writing png %s: %s", img_filename, err.Error()))}
 
 
-  srv.Stop()
+}
+
+
+func BenchmarkConvert( b *testing.B ) {
+
+  srv := lazyfs_testfiles_http_server.HttpServer( 4567 )
+  defer srv.Stop()
+
+  testUrl,err := url.Parse( "http://localhost:4567/" + lazyfs_testfiles.TestMovFile )
+
+  source,err := lazyfs.OpenHttpSource( *testUrl )
+  //fmt.Println(source)
+  //source,err := lazyfs.OpenLocalFileSource( "../go-lazyfs-testfiles/", TestMovPath )
+  if err != nil {
+    panic("Couldn't open HttpFSSource")
+  }
+
+  store,err := lazyfs.OpenSparseFileStore( source, SparseHttpStoreRoot )
+  if store == nil {
+    panic("Couldn't open SparesFileFSStore")
+  }
+
+  b.ResetTimer()
+  for i := 0; i < b.N; i++ {
+  mov := LoadMovMetadata( store )
+
+  // Try extracting a frame
+  frame := 2
+  img,_ := mov.ExtractFrame( frame )
+
+  if err != nil { panic(fmt.Sprintf("Error decoding frame: %s", err.Error()))}
+
+  img_filename := fmt.Sprintf("frame%06d.png", frame)
+  img_file,err := os.Create(img_filename)
+  if err != nil { panic(fmt.Sprintf("Error creating png %s: %s", img_filename, err.Error()))}
+
+  err = png.Encode( img_file, img )
+  if err != nil { panic(fmt.Sprintf("Error writing png %s: %s", img_filename, err.Error()))}
+}
+b.StopTimer()
+
 }
